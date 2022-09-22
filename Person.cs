a49f1adcbbbae61746cdc9a85759e8e44db931b9
GailC7 <chintalekhag@etsu.edu>
2022-09-22 16:54:04 -0400

    Create classes

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Person
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public Address Address { get; init; }
+
+    }
+}
